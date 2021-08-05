7318388910fe1fc34d2e55ea9231b596fe988922
uneec96 <86100199+uneec96@users.noreply.github.com>
2021-08-05 23:37:44 +0530

    Create instances.tf

---
+++ instances.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "web1" {
+  count          = var.instance_count
+  ami            = var.ami
+  instance_type  = var.instance_type
+
+  tags = {
+      Name  = element(var.instance_tags, count.index)
+    }
+}
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "ap-south-1a"
+  size              = 8
+}
+
+resource "aws_volume_attachment" "ebs_att1" {
+  device_name = "/dev/sdh"
+  volume_id   = "${aws_ebs_volume.example.id}"
+  instance_id = "${aws_instance.web1[0].id}"
+}
+resource "aws_volume_attachment" "ebs_att2" {
+  device_name = "/dev/sdh"
+  volume_id   = "${aws_ebs_volume.example.id}"
+  instance_id = "${aws_instance.web1[1].id}"
+}
+resource "aws_volume_attachment" "ebs_att3" {
+  device_name = "/dev/sdh"
+  volume_id   = "${aws_ebs_volume.example.id}"
+  instance_id = "${aws_instance.web1[2].id}"
