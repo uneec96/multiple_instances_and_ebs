c980ec54c73f402762827f6cbc1d786742789af4
uneec96 <86100199+uneec96@users.noreply.github.com>
2021-08-05 23:37:03 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "ami" {
+    default = "ami-00bf4ae5a7909786c"
+}
+
+variable "instance_tags" {
+  type = list
+  default = ["Terraform-1", "Terraform-2"]
+}
+
+variable "instance_type" {
+    default = "t2.micro"  
+}
+
+variable "instance_count" {
+  default = "3"
+}
+
